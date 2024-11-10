1fc451ff6b7894c85412ec3863aef000f5c03220
shakthi2003gh <shakthikumar123456789@gmail.com>
2024-11-11 00:18:21 +0530

    Fix global error handler

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,19 @@ require("dotenv").config();
 require("express-async-errors");
 
 const express = require("express");
+const { error } = require("./common/logger");
+
 const app = express();
+
+process.on("unhandledRejection", (error) => {
+  throw error;
+});
+
+process.on("uncaughtException", (err) => {
+  error("Uncaught Exception:", err);
+
+  process.exit(1);
+});
 
 require("./startup/db")(app);
 require("./startup/cors")(app);
